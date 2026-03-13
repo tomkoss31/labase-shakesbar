18ee78d2b6818b43b63f6be27e05227b3a5471d6
tomkoss31 <tomkoss31@gmail.com>
2026-03-13 17:02:49 +0100

    git add . git commit -m "PWA installable app" git push

---
+++ public/sw.js
@@ -0,0 +1,7 @@
+sw.js
+
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installé");
+});
+
+self.addEventListener("fetch", (event) => {});
--- src/main.tsx
+++ src/main.tsx
@@ -7,3 +7,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>
 );
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js");
+}
